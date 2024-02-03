6b024d604138c5c6d79f3dd0b8eb6b0c01161ac8
Justin Lecas <justin.lecas@gmail.com>
2024-02-03 16:10:13 +0100

    first commit

---
+++ src/test/java/org/laurichapp/serveurconf/ServeurConfApplicationTests.java
@@ -0,0 +1,13 @@
+package org.laurichapp.serveurconf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurConfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
