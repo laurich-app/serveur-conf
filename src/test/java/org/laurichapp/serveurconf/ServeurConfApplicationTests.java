2a543105a4e94a951899ffed71c685de172e76a6
Justin Lecas <justin.lecas@gmail.com>
2024-04-09 18:26:08 +0200

    Ajout du serveur de conf dans Consul

--- src/test/java/org/laurichapp/serveurconf/ServeurConfApplicationTests.java
+++ src/test/java/org/laurichapp/serveurconf/ServeurConfApplicationTests.java
@@ -1,5 +1,6 @@
 package org.laurichapp.serveurconf;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class ServeurConfApplicationTests {
 
     @Test
     void contextLoads() {
+        // Vérification que l'application démarre bien
+        Assertions.assertTrue(true);
     }
 
 }
